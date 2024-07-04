f505bd7aa7cd31e1474a71a55f4573b6277d2c76
oxdjww <oxdjww@gmail.com>
2024-07-04 16:43:57 +0900

    ci: ci test

--- src/main/java/com/hackathon/TimeLapse/test/TestController.java
+++ src/main/java/com/hackathon/TimeLapse/test/TestController.java
@@ -3,11 +3,11 @@ package com.hackathon.TimeLapse.test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@RestController
 public class TestController {
 
 	@GetMapping
 	public String test() {
-		return "test";
+		return "test123";
 	}
 }
