70ee3d5c346dffc7d17898348f51db919083e502
변유진 <byj7176@gmail.com>
2024-07-04 19:01:37 +0900

    [FEAT] image entity 추가 #5

--- src/main/java/com/hackathon/TimeLapse/domain/Article.java
+++ src/main/java/com/hackathon/TimeLapse/domain/Article.java
@@ -3,6 +3,9 @@ package com.hackathon.TimeLapse.domain;
 import com.hackathon.TimeLapse.domain.common.BaseEntity;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Article extends BaseEntity {
     @Id
@@ -19,12 +22,6 @@ public class Article extends BaseEntity {
 
     @Column(
             nullable = false,
-            length = 200
-    )
-    private String image_url;
-
-    @Column(
-            nullable = false,
             length = 400
     )
     private String description;
@@ -36,4 +33,10 @@ public class Article extends BaseEntity {
             name="member_id"
     )
     private Member member;
+
+    @OneToMany(
+            mappedBy = "article",
+            cascade = {CascadeType.ALL}
+    )
+    private List<Image> imageList = new ArrayList<>();
 }
