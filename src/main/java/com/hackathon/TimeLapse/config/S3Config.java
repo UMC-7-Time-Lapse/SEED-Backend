f5df5e44eeacb1a7a7beb7552c8b5400d8461ed6
oxdjww <oxdjww@gmail.com>
2024-07-04 19:14:33 +0900

    fix: S3 환경변수 위치 수정

--- src/main/java/com/hackathon/TimeLapse/config/S3Config.java
+++ src/main/java/com/hackathon/TimeLapse/config/S3Config.java
@@ -11,11 +11,11 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 @Configuration
 public class S3Config {
-	@Value("${cloud.aws.credentials.access-key}")
+	@Value("${cloud.aws.s3.credentials.access-key}")
 	private String accessKey;
-	@Value("${cloud.aws.credentials.secret-key}")
+	@Value("${cloud.aws.s3.credentials.secret-key}")
 	private String secretKey;
-	@Value("${cloud.aws.region.static}")
+	@Value("${cloud.aws.s3.region.static}")
 	private String region;
 
 	@Bean
