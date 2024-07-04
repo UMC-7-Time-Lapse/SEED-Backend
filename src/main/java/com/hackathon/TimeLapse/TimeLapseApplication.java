aa9a60f81c71dfb1a35ab52186df19d76f06577f
oxdjww <oxdjww@gmail.com>
2024-07-05 04:52:19 +0900

    fix: enable updateAt createdAt

--- src/main/java/com/hackathon/TimeLapse/TimeLapseApplication.java
+++ src/main/java/com/hackathon/TimeLapse/TimeLapseApplication.java
@@ -2,8 +2,10 @@ package com.hackathon.TimeLapse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TimeLapseApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/hackathon/TimeLapse/common/BaseEntity.java
+++ src/main/java/com/hackathon/TimeLapse/common/BaseEntity.java
@@ -1,27 +1,30 @@
 package com.hackathon.TimeLapse.common;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
-@EntityListeners({AbstractMethodError.class})
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
-    @CreatedDate
-    private LocalDateTime createdAt;
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
+	@CreatedDate
+	private LocalDateTime createdAt;
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
 
-    public BaseEntity() {
-    }
+	public BaseEntity() {
+	}
 
-    public LocalDateTime getCreatedAt() {
-        return this.createdAt;
-    }
+	public LocalDateTime getCreatedAt() {
+		return this.createdAt;
+	}
 
-    public LocalDateTime getUpdatedAt() {
-        return this.updatedAt;
-    }
+	public LocalDateTime getUpdatedAt() {
+		return this.updatedAt;
+	}
 }
