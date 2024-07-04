4449af7617ed1aad92f8462cabe8fb4882f9c5bd
oxdjww <oxdjww@gmail.com>
2024-07-04 15:18:35 +0900

    feat: 기본 스프링부트 파일을 업로드하다

---
+++ src/test/java/com/hackathon/TimeLapse/TimeLapseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.TimeLapse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeLapseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
